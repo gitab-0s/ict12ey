27134feae1d08f1d92deddcb30d72111bfbb79ad
austriacox <marcdavinciii@gmail.com>
2025-04-29 08:56:23 +0800

    alldone

---
+++ cf1/src/main/java/com/mycompany/cf1/Cf1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cf1;
+
+/**
+ *
+ * @author CL3-PC34
+ */
+public class Cf1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
